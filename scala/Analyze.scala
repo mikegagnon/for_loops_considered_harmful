c16cd6e97a9d9f7b6d0c65552feb99909efd11b2
Mike Gagnon <mike@hellasec.com>
2012-06-24 17:39:57 -0700

    added README

--- scala/Analyze.scala
+++ scala/Analyze.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 HellaSec, LLC
+ * Copyright 2012 Michael N. Gagnon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
